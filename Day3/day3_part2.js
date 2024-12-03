3e2e03147688fc51e86e6b81c1746d586fe51842
Bianca Eni <b.b.eni@student.tue.nl>
2024-12-03 13:54:57 +0100

    :(

--- Day3/day3_part2.js
+++ Day3/day3_part2.js
@@ -10,32 +10,29 @@ function readFile(filePath) {
 }
 
 const adventOfCodeInput = readFile("../Day3/day3input.txt");
+const mulInstructions = /mul\(([0-9]+),([0-9]+)\)/;
+const doInstruction = /do\(\)/;
+const dontInstruction = /don't\(\)/;
 
-const mulInstructions = /mul\(([0-9]+),([0-9]+)\)/g;
-const betweenDosAndDonts = /don't\(\).*do\(\)/g;
+let mulEnabled = true; // enabled
+let totalSum = 0;
+const tokens = adventOfCodeInput.match(/mul\([^\)]+\)|do\(\)|don't\(\)/g);
+console.log(tokens);
 
-function getMults(string) {
-  const instructions = string.matchAll(mulInstructions);
-  let multiply = 0;
-
-  for (let instruction of instructions) {
-    multiply += parseInt(instruction[1]) * parseInt(instruction[2]);
+for (let token of tokens) {
+  if (token.startsWith("mul(")) {
+    if (mulEnabled) {
+      const match = token.match(mulInstructions);
+      if (match) {
+        console.log(match, match[1], match[2]);
+        totalSum += match[1] * match[2];
+      }
+    }
+  } else if (doInstruction.test(token)) {
+    mulEnabled = true;
+  } else if (dontInstruction.test(token)) {
+    mulEnabled = false;
   }
-  return multiply;
 }
 
-let totalSum = getMults(adventOfCodeInput);
-
-const forbiddenInstructions = adventOfCodeInput.matchAll(betweenDosAndDonts);
-
-let forbiddenSum = 0;
-array = Array.from(forbiddenInstructions);
-
-for (let instruction of array) {
-  console.log(instruction[0],'\n\n\n');
-  forbiddenSum += getMults(instruction[0]);
-  //   console.log("Mul:", getMults(instruction[0]));
-}
-
-let goodSum = totalSum - forbiddenSum;
-console.log(goodSum);
+console.log("Final sum:", totalSum);
