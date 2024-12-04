1e8c23b3f771f4e40d7e89d07b9c4683d24d263c
Bianca Eni <b.b.eni@student.tue.nl>
2024-12-04 11:44:02 +0100

    day4 part2

--- Day4/day4_part2.js
+++ Day4/day4_part2.js
@@ -11,4 +11,21 @@ function readFile(filePath) {
 
 const adventOfCodeInput = readFile("../Day4/day4input.txt");
 const matrix = adventOfCodeInput.split("\n").map((row) => row.split(""));
+const len = matrix.length;
+let xmas = 0;
 
+for (let i = 1; i < len-1; i++) {
+  for (let j = 1; j< len-1; j++) {
+    if (matrix[i][j] == "A") {
+      if (
+        ((matrix[i - 1][j - 1] == "M" && matrix[i + 1][j + 1] == "S") ||
+          (matrix[i - 1][j - 1] == "S" && matrix[i + 1][j + 1] == "M")) &&
+        ((matrix[i - 1][j + 1] == "M" && matrix[i + 1][j - 1] == "S") ||
+          (matrix[i - 1][j + 1] == "S" && matrix[i + 1][j - 1] == "M"))
+      ) {
+        xmas++;
+      }
+    }
+  }
+}
+console.log(xmas);
