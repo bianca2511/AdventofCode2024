25209d845d15511e04347555bdbba99a703e5c7d
Bianca Eni <b.b.eni@student.tue.nl>
2024-12-09 09:49:32 +0100

    day 7 part2  fr

--- Day7/day7_part2.js
+++ Day7/day7_part2.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const fs = require("fs");
 
 /**
@@ -20,42 +22,47 @@ const adventOfCodeInput = readFile("../Day7/day7input.txt");
 const lines = adventOfCodeInput.split("\n");
 
 /**
- * @param {string | bigint} result
+ * @param {bigint} result
  * @param {string | any[]} terms
  * @param {number} index
  * @param {bigint} currentSum
  * @returns {boolean}
  */
+
 function checkMath(result, terms, index, currentSum) {
-  if (index == terms.length) {
-    return currentSum == BigInt(result);
+  if (index === terms.length) {
+    return currentSum === result;
+  }
+
+  if (currentSum > result) {
+    return false;
   }
 
   const term = terms[index];
-  const concatValue = BigInt(currentSum + term);
-  if (checkMath(result, terms, index + 1, concatValue)) {
-    return true;
-  }
-
-  if (checkMath(result, terms, index + 1, currentSum + BigInt(term))) {
-    return true;
-  }
-
-  if (checkMath(result, terms, index + 1, currentSum * BigInt(term))) {
-    return true;
-  }
-
-  return false;
+  const termString = term.toString();
+  return (
+    checkMath(
+      result,
+      terms,
+      index + 1,
+      BigInt(currentSum.toString() + termString)
+    ) ||
+    checkMath(result, terms, index + 1, currentSum + term) ||
+    checkMath(result, terms, index + 1, currentSum * term)
+  );
 }
 
 let sum = 0n;
 
 for (let line of lines) {
-  let result = line.split(":")[0];
-  let terms = line.split(":")[1].split(" ").slice(1);
-  //   console.log(result, terms);
+  let result = BigInt(line.split(":")[0]);
+  let terms = line
+    .split(":")[1]
+    .split(" ")
+    .slice(1)
+    .map((element) => BigInt(element));
 
-  if (checkMath(result, terms, 0, 0n)) {
+  if (checkMath(result, terms, 1, terms[0])) {
     sum += BigInt(result);
     // console.log(result);
   }
